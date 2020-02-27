30f61b95b442f9757d7766cfaa964dd0af7f4c0e
sangjune <june11215@naver.com>
2020-02-28 08:31:50 +0900

    simple_test

--- PRIVATE_1/src/Pack/test.java
+++ PRIVATE_1/src/Pack/test.java
@@ -1,12 +1,34 @@
 package Pack;
 
+import java.util.Scanner;
+
 public class test {
 
 	public static void main(String[] args) {
-
-
-		System.out.println("Hello Git!");
-
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		int input[][]=new int[T][10];
+		
+		for(int i=0;i<T;i++) 
+			for(int j=0;j<10;j++){
+			input[i][j]=sc.nextInt();
+		}
+		
+		for(int test_case = 0; test_case < T; test_case++) {
+			int sum = 0;
+			for(int j=0;j<10;j++)
+		{
+		
+	     
+		if(input[test_case][j]%2==1)
+			sum += input[test_case][j];
+		
+		
+		}
+			System.out.println("#"+(test_case+1)+" "+sum);
+		}
 	}
 
 }
